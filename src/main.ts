c644ee50c730193959560eac9cfa976171b11eb6
A. Pechenkin <pechekin685@gmail.com>
2025-12-23 17:18:39 +0700

    фикс jwt авторизации

--- src/main.ts
+++ src/main.ts
@@ -7,19 +7,19 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api');
   app.use(cookieParser());
-  app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'http://127.0.0.1:3000',
-      'http://localhost:3001',
-      'http://localhost:3333',
-      'http://192.168.0.243:3000',
-    ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    credentials: true,
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-  });
+  // app.enableCors({
+  //   origin: [
+  //     'http://localhost:3000',
+  //     'http://127.0.0.1:3000',
+  //     'http://localhost:3001',
+  //     'http://localhost:3333',
+  //     'http://192.168.0.243:3000',
+  //   ],
+  //   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  //   credentials: true,
+  //   preflightContinue: false,
+  //   optionsSuccessStatus: 204,
+  // });
   await app.listen(process.env.PORT ?? 3333);
 }
 
